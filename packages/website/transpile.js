d6903648291e7a9344d81afbe9071ea38d646db5
Cody Jones <magerx1794@gmail.com>
2019-05-12 16:04:45 -0500

    Removed console logging from transpile

--- packages/website/transpile.js
+++ packages/website/transpile.js
@@ -1,30 +1,26 @@
-
 const transpileDeps = [];
 const transpileNestedDeps = [];
 const noTranspile = [];
 
-//NoTranspileJson
 var ntpjson = require('./transpile.json')
 var packagejson = require('./package.json')
-console.log(ntpjson);
+
 function AddTranspile(item) {
-			//console.log(item);
 			if(!transpileDeps.includes(item)) {
 					transpileDeps.push(item);	
 			}
 }
-console.log("NTPJson");
+
 Object.keys(ntpjson.transpile).forEach(function (item) {
 			AddTranspile(ntpjson.transpile[item]);	
 	});
-console.log("");
-console.log("packagejson");
+
 Object.keys(packagejson.dependencies).forEach(function (item) {
 	
 	if(!ntpjson.noTranspile.includes(item)) {
 		AddTranspile(item);	
 	}
 });
-console.log("");
-console.log(transpileDeps);
+
+
 module.exports = transpileDeps;
