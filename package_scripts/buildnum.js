310f1fda62faec0426d8ce7261de96fd51f8a248
Cody Jones <magerx1794@gmail.com>
2019-05-31 18:41:48 -0500

    test

---
+++ package_scripts/buildnum.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const { generate, validate, parse, format } = require('build-number-generator')
+
+let packagejson = fs.readFileSync('../package.json');
+packagejson = JSON.parse(packagejson);
+
+var build = 0;
+let bkgBuild = packagejson.build;
+try {
+	console.log(build);
+	if(!bkgBuild.includes("-b")) {
+		console.log("test");
+		throw new exception;
+	}
+
+	let obj = bkgBuild.split("-b");
+	let [origDate, origBuild] = obj;
+	build = parseInt(origBuild) + 1;
+
+}
+catch {
+	build = 0;
+	let newBuild = parseInt(bkgBuild);
+	if(!isNaN(newBuild)) {
+		build = newBuild;
+	}
+}
+const buildDate = generate().substr(0,6);
+
+const buildNumber = buildDate + "-b" + build;
+
+
+packagejson.build = buildNumber;
+
+console.log("New Build: " + buildNumber);
+
+
+let data = JSON.stringify(packagejson, null, 4);
+fs.writeFileSync('../package.json', data);
